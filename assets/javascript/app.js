e36b452fb298d928763f6151908ab13d2df8643f
pholmes10 <pholmes10@gmail.com>
2018-11-21 09:02:37 -0600

    added functionality for adding buttons and some basic styling

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+$('#submit').on('click', function (event) {
+    event.preventDefault();
+    var userInput = $("#search").val();
+    console.log(userInput);
+    var newBtn = $("<button>").attr("data-search", userInput);
+    newBtn.text(userInput);
+    console.log(newBtn);
+    $(".rowTop").append(newBtn);
+});
+
+$('button').on('click', function () {
+    var name = $(this).data("search");
+
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=eMxiubUBiYXeaZPLMjMQyxAVkubINTVG&limit=10"
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response);
+        for (var i = 0; i < response.data.length; i++) {
+            $("#gifDiv").prepend("<p>Rating: " + response.data[i].rating + "</p>");
+            $("#gifDiv").prepend("<img src='" + response.data[i].images.fixed_height.url + "'>");
+        };
+    });
+});
+
