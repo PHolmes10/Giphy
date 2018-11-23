8a70d5f67b4f9a087bfb7bc011115b22796951ab
pholmes10 <pholmes10@gmail.com>
2018-11-23 00:14:19 -0500

    added comments to JS file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,81 @@
+// Click event for the submit button
 $('#submit').on('click', function (event) {
+
+    // Preventing the default action of the submit button.
     event.preventDefault();
-    var userInput = $("#search").val();
-    // console.log(userInput);
+    // creating a variable to capture the value of the search form.
+    var userInput = $("#search").val().trim();
+    // creating a new button with a custom attribute that matches what the user types into the search form.
     var newBtn = $("<button>").attr("data-search", userInput);
+    // Adding text to the button to match what the user typed in the search form.
     newBtn.text(userInput);
-    // console.log(newBtn);
+    // Appending the new button to the bar of buttons at the top of the page
     $(".rowTop").append(newBtn);
+    // Clearing the search form 
     $("#search").val("");
 });
 
+// Click event for each button using event delegation
 $(document).on('click', 'button', function () {
+
+    // creating a variable to capture the value of the data-search attribute of the button that was clicked on 
     var name = $(this).data("search");
-
+    // URL used to reference the giphy API based on the name
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=eMxiubUBiYXeaZPLMjMQyxAVkubINTVG&limit=10"
-
+    // AJAX call to get data from the giphy API
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).then(function (response) {
-        // console.log(response);
-        var results = response.data;
-        for (var i = 0; i < results.length; i++) {
-            $("#gifDiv").prepend("<p>Rating: " + response.data[i].rating + "</p>");
-            var images = $("<img src='" + response.data[i].images.fixed_height_still.url + "'>");
-            images.attr("class", "gif");
-            images.attr("data-state", "still");
-            images.attr("data-still", response.data[i].images.fixed_height_still.url);
-            images.attr("data-animate", response.data[i].images.fixed_height.url);
 
-            $("#gifDiv").prepend(images);
+        // Creating variable to capture the data list of the response object (list of 10 random gifs)
+        var results = response.data;
+        // Loop through the list of 10 random gifs
+        for (var i = 0; i < results.length; i++) {
+            // Create a new div for each gif
+            var gifDiv = $("<div>");
+            // Capture the rating of each gif
+            var rating = response.data[i].rating;
+            // Creating a p tag to display the rating 
+            var p = $("<p>").text("Rating: " + rating);
+            // Creating a variable to display the gif as a still image
+            var images = $("<img src='" + response.data[i].images.fixed_height_still.url + "'>");
+            // Adding a class of gif to each gif
+            images.attr("class", "gif");
+            // Adding a custom attribute called data-state and setting it to "still" so that the gifs are still upon opening the page
+            images.attr("data-state", "still");
+            // Adding a custom attribute called data-still and setting the value to the link of the still gif image
+            images.attr("data-still", response.data[i].images.fixed_height_still.url);
+            // Adding a custom attribute called data-animate and setting the value to the link of the moving gif image
+            images.attr("data-animate", response.data[i].images.fixed_height.url);
+            // Appending the P and Image tags to the div
+            gifDiv.prepend(p);
+            gifDiv.prepend(images);
+            // Appending the gif div to a div on the DOM 
+            $("#gifList").prepend(gifDiv);
         };
 
     });
 });
 
+// Click event for each gif image (event delegation used here as well)
 $(document).on("click", ".gif", function () {
+    // New variable to capture the data-state value of the image being clicked on
     var state = $(this).attr("data-state");
-    console.log(this);
+    // 'If' statement checking to see if data-state attribute is 'still'
     if (state === "still") {
-        // this.src = data-animate
+        // creating variable to capture the data-animate value of the image being clicked on
         var animate = $(this).attr("data-animate");
+        // If the state is 'still', when the image is clicked change the src value to the data-animate value
         $(this).attr("src", animate);
+        // If the state is 'still', when the image is clicked change the data-state value to 'animate'
         $(this).attr("data-state", "animate");
     } else {
+        // creating variable to capture the data-still value of the image being clicked on
         var still = $(this).attr("data-still");
+        // If the state is 'animate', when the image is clicked change the src value to the data-still value
         $(this).attr("src", still);
+        // If the state is 'animate', when the image is clicked change the data-state value to 'still'
         $(this).attr("data-state", "still");
     };
 });
